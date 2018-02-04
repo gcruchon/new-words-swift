ecf809d28e5bceb1939b85afe6d92e62785bb574
Gilles Cruchon <gilles.cruchon@axa.fr>
2018-02-04 12:59:37 +0100

    chore: Init the project

---
+++ NewWords/NewWords/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewWords
+//
+//  Created by Gilles Cruchon on 04/02/2018.
+//  Copyright © 2018 Gilles Cruchon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
